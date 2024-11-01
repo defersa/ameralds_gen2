df72f205f7600c9b9ffe32e35c5dc9d33101874a
defersa <darkbotone@gmail.com>
2024-11-02 01:58:30 +0300

    v0.1.19 Add untracked files

---
+++ front/src/app/api/models/EnumApiEntityNames.ts
@@ -0,0 +1,14 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * Error entity name
+ */
+export enum EnumApiEntityNames {
+    CATEGORY = 'category',
+    SIZE = 'size',
+    PATTERN = 'pattern',
+    PATTERN_SIZE = 'patternSize',
+    FILE = 'file',
+}
---
+++ front/src/app/api/models/PatternSizeDto.ts
@@ -0,0 +1,31 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type PatternSizeDto = {
+    /**
+     * Id of entity
+     */
+    id: number;
+    /**
+     * Size id
+     */
+    size: number;
+    /**
+     * Cbb file id
+     */
+    cbb: number;
+    /**
+     * Pdf file id
+     */
+    pdf: number;
+    /**
+     * Png file id
+     */
+    png: number;
+    /**
+     * Jbb file id
+     */
+    jbb: number;
+};
+
